d6ab060605876729288c94c7801414b25b71391b
Akintade <franmalae851@gmail.com>
2025-11-04 11:19:27 +0200

    Add MySQL SKU and version variables

--- variable.tf
+++ variable.tf
@@ -23,3 +23,14 @@ variable "mysql_admin_password" {
   sensitive   = true
   default     = "P@ssw0rd1234!"
 }
+variable "mysql_sku" {
+  description = "The SKU name for the MySQL Flexible Server."
+  type        = string
+  default     = "B_Standard_B1ms"
+}
+
+variable "mysql_version" {
+  description = "The MySQL server version."
+  type        = string
+  default     = "8.0.21"
+}
