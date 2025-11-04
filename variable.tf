a44a606a4329ebf5b28fd8ef996975dc6177238e
Akintade <franmalae851@gmail.com>
2025-11-04 12:27:32 +0200

    Add missing variables for Terraform validation

--- variable.tf
+++ variable.tf
@@ -1,4 +1,37 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  default     = "epicbook-rg"
+}
+
+variable "location" {
+  description = "Azure region"
+  default     = "East US"
+}
+
+variable "admin_username" {
+  description = "Admin username for VMs"
+  default     = "azureuser"
+}
+
 variable "ssh_public_key_path" {
   description = "Path to SSH public key"
   default     = "C:/Users/Samuel/.ssh/epicbook_key.pub"
+}
+
+variable "mysql_admin_password" {
+  description = "MySQL admin password"
+  sensitive   = true
+  default     = "P@ssw0rd1234!"
+}
+
+variable "mysql_sku" {
+  description = "The SKU name for the MySQL Flexible Server."
+  type        = string
+  default     = "B_Standard_B1ms"
+}
+
+variable "mysql_version" {
+  description = "The MySQL server version."
+  type        = string
+  default     = "8.0.21"
 }
